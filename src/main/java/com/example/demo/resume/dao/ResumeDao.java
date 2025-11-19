d4b3d4c8a4dd4fb46d87ca7e9c5ce79b1fa8a50f
Addilete <165387997+adillette@users.noreply.github.com>
2025-11-19 14:50:26 +0900

    feat: 이력서 dao, xml 파일 완료

---
+++ src/main/java/com/example/demo/resume/dao/ResumeDao.java
@@ -0,0 +1,30 @@
+package com.example.demo.resume.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+
+import com.example.demo.resume.entity.Resume;
+
+@Mapper
+public interface ResumeDao {
+
+  public int insertResume(Resume resume);//이력서 생성
+
+  public Resume selectResumeById(int resumeId);//이력서 상세 조회
+
+  public List<Resume> selectResumesByMemberId(int memberId);//회원별 이력서 목록 조회
+
+  public int updateResume(Resume resume);//이력서 수정 (전체)
+
+  public int updateResumeFeedback(
+    @Param("resumeId") Integer resumeId,
+    @Param("feedback") String feedback);//AI 피드백만 업데이트
+
+  public int deleteResume(int resumeId);//이력서 삭제
+
+  public List<Resume> searchResumes(@Param("memberId") Integer memberId,
+      @Param("keyword") String keyword);//이력서 검색 (제목, 내용)
+
+}
---
+++ src/main/java/com/example/demo/resume/entity/Resume.java
@@ -0,0 +1,24 @@
+package com.example.demo.resume.entity;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+@Data
+public class Resume {
+  private Integer resumeId; //이력서 ID
+  private Integer memberId;  //사용사ID
+  private Integer interviewDocId; //면접 서류 ID
+  private String title; // 이력서 제목
+  private String careerInfo; //경력
+  private String educationInfo;//이력
+  private String skills;//기술
+  private String certificates;//자격증
+  private String awards;//수상
+  private String activity;//활동
+  private String resumeFeedback; //이력서 피드백
+  private LocalDateTime createdAt;//생성일
+  private LocalDateTime updatedAt;//수정일
+
+
+}
