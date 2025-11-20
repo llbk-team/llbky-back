d8d0daf00db70553165f3830bda65df9867d4def
bale0610 <see23069@naver.com>
2025-11-20 16:10:51 +0900

    feat: 키워드 저장 entity,dao,xml 추가

---
+++ src/main/java/com/example/demo/newstrend/dao/SavedKeywordDao.java
@@ -0,0 +1,22 @@
+package com.example.demo.newstrend.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Mapper;
+
+import com.example.demo.newstrend.entity.SavedKeyword;
+
+@Mapper
+public interface SavedKeywordDao {
+  // 키워드 저장
+  public int insertSavedKeyword(SavedKeyword savedKeyword);
+
+  // 멤버 키워드 전체 조회
+  public List<SavedKeyword> selectSavedKeywordListByMemberId(int memberId);
+
+  // 키워드 중복 체크
+  public SavedKeyword selectSavedKeyword(int memberId, String keyword);
+
+  // 삭제
+  public int deleteSavedKeyword(int savedKeywordId);
+}
--- src/main/java/com/example/demo/newstrend/entity/SavedKeyword.java
+++ src/main/java/com/example/demo/newstrend/entity/SavedKeyword.java
@@ -9,6 +9,6 @@ public class SavedKeyword {
   private Integer savedKeywordId; // 저장한 키워드 ID
   private Integer memberId; // 멤버 ID
   private String keyword; // 키워드
-  private String source_label; // 직접추가, 추천 직무 태그 표시용
+  private String sourceLabel; // 직접추가, 추천 직무 태그 표시용
   private LocalDateTime createdAt; // 생성일
 }
