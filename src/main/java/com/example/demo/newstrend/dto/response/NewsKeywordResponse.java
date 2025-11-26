01320ef9be422d4c1b5396908c727fda96beaa5b
Addilete <165387997+adillette@users.noreply.github.com>
2025-11-27 04:52:14 +0900

    fix: 뉴스 뉴스안에 요약용 키워드 response 필드 항목 수정함

--- src/main/java/com/example/demo/newstrend/dto/response/NewsKeywordResponse.java
+++ src/main/java/com/example/demo/newstrend/dto/response/NewsKeywordResponse.java
@@ -5,6 +5,5 @@ import lombok.Data;
 @Data
 public class NewsKeywordResponse {
     private String keyword;
-    private String type;        // 직무, 기술, 산업, 기타
-    private Double importanceScore;
+   
 }
