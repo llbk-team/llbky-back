d5c4c38923607e0faea3d59ad509cced91da0087
Addilete <165387997+adillette@users.noreply.github.com>
2025-11-27 04:52:55 +0900

    feat: 뉴스 수집할때 사용하는 요청용 클래스

---
+++ src/main/java/com/example/demo/newstrend/dto/request/NewsCollectRequest.java
@@ -0,0 +1,15 @@
+package com.example.demo.newstrend.dto.request;
+
+import java.util.List;
+
+import lombok.Data;
+
+@Data
+public class NewsCollectRequest {
+   
+    private List<String> keywords;
+    
+    private Integer memberId = 1;  // 기본값
+    
+    private Integer limitPerKeyword = 1;  // 키워드별 수집 개수
+}
