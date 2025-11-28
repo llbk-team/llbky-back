93135878d76a83eac79178c690def6126ba78283
kyum22n <aphrod207@naver.com>
2025-11-28 13:36:42 +0900

    feat: 면접 답변 재제출 기능 추가

--- src/main/java/com/example/demo/interview/controller/InterviewController.java
+++ src/main/java/com/example/demo/interview/controller/InterviewController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.example.demo.interview.dao.InterviewAnswerDao;
 import com.example.demo.interview.dto.response.AnswerFeedbackResponse;
 import com.example.demo.interview.dto.response.QuestionResponse;
 import com.example.demo.interview.dto.response.SaveSessionResponse;
@@ -22,13 +21,10 @@ import com.example.demo.interview.service.InterviewService;
 @RestController
 @RequestMapping("/interview")
 public class InterviewController {
- // Service
-@Autowired
-private InterviewService interviewService;
 
-// DAO
-@Autowired
-private InterviewAnswerDao interviewAnswerDao;
+    // Service
+    @Autowired
+    private InterviewService interviewService;
 
     @PostMapping("/ai-questions")
     public ResponseEntity<List<QuestionResponse>> createQuestion(
@@ -71,6 +67,21 @@ private InterviewAnswerDao interviewAnswerDao;
     ) throws Exception {
 
         return ResponseEntity.ok(interviewService.createInterviewAnswer(questionId, audio, video));        
+    }
+    
+    // 답변 다시 제출============================================================================================================================================
+    @PostMapping(
+        value = "/re-submit-answer",
+        consumes = MediaType.MULTIPART_FORM_DATA_VALUE
+    )
+    public ResponseEntity<Integer> reSubmitAnswer(
+        @RequestParam("answerId") int answerId,
+        @RequestParam(value = "audio", required = false) MultipartFile audio,
+        @RequestParam(value = "video", required = false) MultipartFile video,
+        @RequestParam(value = "frames", required = false) List<MultipartFile> frames
+    ) throws Exception {
+
+        return ResponseEntity.ok(interviewService.modifyInterviewAnswer(answerId, audio, video));        
     }
     
     // 답변별 AI 피드백 생성===================================================================================================================
