918a38dcf67b74847f8d36dc8f003268650ed600
bale0610 <see23069@naver.com>
2025-12-03 11:00:45 +0900

    fix: 직무 인사이트 코드 수정

--- src/main/java/com/example/demo/newstrend/dto/response/JobInsightResponse.java
+++ src/main/java/com/example/demo/newstrend/dto/response/JobInsightResponse.java
@@ -7,6 +7,7 @@ import lombok.Data;
 @Data
 public class JobInsightResponse {
   private String jobRole; // 직무
+  private String summary; // 직무 요약
   private String trendSummary; // 트렌드 요약
   private List<String> relatedKeywords; // 관련 키워드 목록
 }
