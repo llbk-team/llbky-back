660ddc85b5e3e54417aad7cf2efa20bb66d02ede
bale0610 <see23069@naver.com>
2025-12-08 13:27:57 +0900

    fix: 직무 인사이트 주석 추가

--- src/main/java/com/example/demo/newstrend/controller/JobInsightController.java
+++ src/main/java/com/example/demo/newstrend/controller/JobInsightController.java
@@ -18,21 +18,25 @@ public class JobInsightController {
   @Autowired
   private JobInsightService jobInsightService;
 
+  // 직무 인사이트 분석
   @PostMapping("/generate")
   public ResponseEntity<JobInsight> generateJobInsight(@RequestParam("memberId") int memberId) throws Exception {
     return ResponseEntity.ok(jobInsightService.createJobInsight(memberId));
   }
 
+  // 최신 직무 인사이트 1개 조회
   @GetMapping("/latest")
   public ResponseEntity<JobInsight> getJobInsight(@RequestParam("memberId") int memberId) {
     return ResponseEntity.ok(jobInsightService.getLatestInsight(memberId));
   }
 
-  @GetMapping("/today")
+  // 조회시 생성 기간 7일 지날시 재분석
+  @GetMapping("/week")
   public ResponseEntity<JobInsight> analyze(@RequestParam("memberId") int memberId) throws Exception {
     return ResponseEntity.ok(jobInsightService.getOrCreateInsight(memberId));
   }
 
+  // 키워드 저장/삭제 시 성장 제안 수정
   @PostMapping("/refresh")
   public ResponseEntity<JobInsight> updateGrowth(@RequestParam("memberId") int memberId) throws Exception {
     return ResponseEntity.ok(jobInsightService.modifyGrowthAnalysis(memberId));
